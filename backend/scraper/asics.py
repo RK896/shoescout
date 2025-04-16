d1305a7bebd51a91c30c8addc7cfd6727afce30f
rohit <rohit.karnik@outlook.com>
2025-04-16 13:47:02 -0400

    using selenium on Asics website. Used presence of element method to take care of pop-up

--- backend/scraper/asics.py
+++ backend/scraper/asics.py
@@ -1,26 +1,49 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def asics_scraper():
-    url = "https://www.asics.com/us/en-us/mens-running-shoes/c/aa10201000/?start=0&sz=50"
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+    url = "https://www.asics.com/us/en-us/mens-running-shoes/c/aa10201000/?start=0&sz=350"
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--disable-gpu") 
+    chrome_options.add_argument("--blink-settings=imagesEnabled=false")  
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64" +
+                                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
 
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.set_page_load_timeout(30)
+    driver.get(url)
+    
+    try:
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'OK')]"))
+        )
+        accept_button = driver.find_element(By.XPATH, "//button[contains(text(), 'OK')]")
+        accept_button.click()  # Accept the pop-up
+        print("Pop-up accepted")
+    except Exception as e:
+        print("No pop-up or pop-up already accepted")
+    WebDriverWait(driver, 10)
+    
 
+    products = driver.find_elements(By.CLASS_NAME, "grid-tile")
+    
     shoes = []
 
-    for shoe in soup.findAll("li", class_="product-tile"):
-        name_tag = shoe.find("div", class_= "product-tile__text")
-        price_tag = shoe.find("span", class_="price-sales")
-        link_tag = shoe.find("a", class_="product-tile__link")
+    for shoe in products:
+        
+        name_tag = shoe.find_element(By.CSS_SELECTOR, ".product-tile__text.product-tile__text--large")
+        price_tag = shoe.find_element(By.CLASS_NAME, "price-sales")
+        link_tag = driver.find_elements(By.CLASS_NAME, "product-tile__link")
 
-        if name_tag and price_tag and link_tag:
+        try:
             name = name_tag.text.strip()
             price = price_tag.text.strip()
-            link = link_tag["href"]
+            link = link_tag.get_attribute("href")
 
             shoes.append({
                 "model": name,
@@ -29,6 +52,10 @@ def asics_scraper():
                 "brand": name.split()[0],
                 "retailer": "Asics"
             })
+        except Exception as e:
+            print(f"Skipping product due to missing info {e}")
+
+    driver.quit()
     return shoes
 
 
