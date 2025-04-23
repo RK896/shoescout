c6fce2778c36b57e3ab0686c08df657329cae967
rohit <rohit.karnik@outlook.com>
2025-04-23 19:52:07 -0400

    started with react and vite to build frontend. Created basic search bar and added some styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+import mockShoes from './mockShoes'
+import { useState } from 'react'
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const filteredShoes = mockShoes.filter(shoe =>
+    shoe.model.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    shoe.brand.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+
+  return (
+
+    <div className='app-container'>
+      <h1>ShoeScout 🏃‍♂️</h1>
+      <input
+        type="text" 
+        placeholder='Search by model or brand'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className='search-bar'
+        />
+
+
+      <div className='shoe-grid'>
+        {filteredShoes.map((shoe, index) => (
+          <div key = {index} className='shoe-card'>
+            <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
+            <h2>{shoe.model}</h2>
+            <p><strong>Brand:</strong> {shoe.brand}</p>
+            <ul>
+              {shoe.retailers.map((r,i) =>(
+                <li key = {i}>
+                  <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
