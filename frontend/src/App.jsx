693b9bb2814153618e461abdb87397c17d7f1d6a
rohit <rohit.karnik@outlook.com>
2025-04-24 08:35:52 -0400

    minor updates to improve styling. Next steps is to finsih styling the website then link with mongoDB

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,9 +12,9 @@ function App() {
 
 
   return (
-
     <div className='app-container'>
       <h1>ShoeScout 🏃‍♂️</h1>
+      <h2>Find the best deals on running shoes!</h2>
       <input
         type="text" 
         placeholder='Search by model or brand'
@@ -23,22 +23,23 @@ function App() {
         className='search-bar'
         />
 
-
-      <div className='shoe-grid'>
-        {filteredShoes.map((shoe, index) => (
-          <div key = {index} className='shoe-card'>
-            <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
-            <h2>{shoe.model}</h2>
-            <p><strong>Brand:</strong> {shoe.brand}</p>
-            <ul>
-              {shoe.retailers.map((r,i) =>(
-                <li key = {i}>
-                  <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+      <div className='container'>
+        <div className='shoe-grid'>
+          {filteredShoes.map((shoe, index) => (
+            <div key = {index} className='shoe-card'>
+              <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
+              <h2>{shoe.model}</h2>
+              <p><strong>Brand:</strong> {shoe.brand}</p>
+              <ul>
+                {shoe.retailers.map((r,i) =>(
+                  <li key = {i}>
+                    <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
