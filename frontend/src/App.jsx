535a78d4b2705ff1f49db8eb5a40f5d487597eec
rohit <rohit.karnik@outlook.com>
2025-04-25 18:51:09 -0400

    hooked up fronted to database and updated css for website display. Still some issues with website formatting and need to scrape pictures

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
-import mockShoes from './mockShoes'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
+  const [shoes, setShoes] = useState([])
 
-  const filteredShoes = mockShoes.filter(shoe =>
+  const filteredShoes = shoes.filter(shoe =>
     shoe.model.toLowerCase().includes(searchTerm.toLowerCase()) ||
     shoe.brand.toLowerCase().includes(searchTerm.toLowerCase())
   )
+
+  useEffect(() => {
+    fetch("http://localhost:8000/shoes")
+      .then(res => res.json())        
+      .then(data => setShoes(data))
+      .catch(err => console.error("Failed to fetch shoes:", err));  
+  }, []);
+  
 
 
   return (
@@ -17,29 +25,29 @@ function App() {
       <h2>Find the best deals on running shoes!</h2>
       <input
         type="text" 
+        id = "search-input"
+        name = "search"
         placeholder='Search by model or brand'
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         className='search-bar'
         />
 
-      <div className='container'>
-        <div className='shoe-grid'>
-          {filteredShoes.map((shoe, index) => (
-            <div key = {index} className='shoe-card'>
-              <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
-              <h2>{shoe.model}</h2>
-              <p><strong>Brand:</strong> {shoe.brand}</p>
-              <ul>
-                {shoe.retailers.map((r,i) =>(
-                  <li key = {i}>
-                    <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+      <div className='shoe-grid'>
+        {filteredShoes.map((shoe, index) => (
+          <div key = {index} className='shoe-card'>
+            <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
+            <h2>{shoe.model}</h2>
+            <p><strong>Brand:</strong> {shoe.brand}</p>
+            <ul>
+              {shoe.retailers.map((r,i) =>(
+                <li key = {i}>
+                  <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   )
