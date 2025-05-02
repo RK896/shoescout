6ec22954ba12b99abdb083f5b6436088203af05a
rohit <rohit.karnik@outlook.com>
2025-05-02 17:33:51 -0400

    used selenium for runningwarehouse scraper as images are lazy loaded. Also fixed a bug with the header sizing depending on the number of shoes on the page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,37 +20,44 @@ function App() {
 
 
   return (
-    <div className='app-container'>
+    <div className='app'>
       <div className='header'>
-      <h1>Shoe Scout 🏃‍♂️</h1>
-      <h2>Find the best deals on running shoes!</h2>
-      <input
-        type="text" 
-        id = "search-input"
-        name = "search"
-        placeholder='Search by model or brand'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className='search-bar'
+        <h1>Shoe Scout 🏃‍♂️</h1>
+        <h2>Find the best deals on running shoes!</h2>
+        <h3>Updated Daily</h3>
+        <input
+          type="text" 
+          id = "search-input"
+          name = "search"
+          placeholder='Search by model or brand'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className='search-bar'
         />
       </div>
-
-      <div className='shoe-grid'>
-        {filteredShoes.map((shoe, index) => (
-          <div key = {index} className='shoe-card'>
-            <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
-            <h2>{shoe.model}</h2>
-            <p><strong>Brand:</strong> {shoe.brand}</p>
-            <p><strong>Retailers:</strong></p>
-            <ul>
-              {shoe.retailers.map((r,i) =>(
-                <li key = {i}>
-                  <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+      <div className='app-container'>
+        <div className='shoe-grid'>
+          {filteredShoes.length === 0 && (
+            <p style={{ textAlign: 'center', fontSize: '1.2rem', color: '#666' }}>
+              No shoes found. Try a different search.
+            </p>
+          )}
+          {filteredShoes.map((shoe, index) => (
+            <div key = {index} className='shoe-card'>
+              <img src={shoe.image} alt={shoe.model} className='shoe-img'/>
+              <h2>{shoe.model}</h2>
+              <p><strong>Brand:</strong> {shoe.brand}</p>
+              <p><strong>Retailers:</strong></p>
+              <ul>
+                {shoe.retailers.map((r,i) =>(
+                  <li key = {i}>
+                    <strong>{r.retailer}</strong>: {r.price} - <a href={r.link} target = "_blank">Buy</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
